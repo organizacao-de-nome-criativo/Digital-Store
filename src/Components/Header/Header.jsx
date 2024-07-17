ecd8c0da14218fd7d91a864baf24bfecff5e01ff
jhonnatan <jhonnatan1>
2024-07-17 12:05:08 -0300

    Base do header

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import "./Header.css";
+export const Header = () => {
+  return (
+    <header className="flex-section-head">
+      <section className="head-main">
+        <div className="logo-img">
+        <img src="src/assets/logo-digital-store.png" alt="" />
+        </div>
+        <div className="search-input">
+          <input type="text" name="" id="" placeholder="Pesquisar produto..."/>
+        </div>
+        
+      </section>
+      <header className="head-navigation">
+        
+      </header>
+    </header>
+  );
+}
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import { Header } from "../../Components/Header/Header";
+export function Home() {
+  
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
