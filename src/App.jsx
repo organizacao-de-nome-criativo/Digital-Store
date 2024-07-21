dd3231dc3fc033923ee2b083c9a93069757ea6f9
lucas <lucassidpaula@gmail.com>
2024-07-21 12:58:07 -0300

    reparação-bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom/cjs/react-router-dom.min";
 import { Home } from "../src/Pages/Home/Home";
 import { Products } from "../src/Pages/Products/Products";
 import { Categories } from "../src/Pages/Categories/Categories";
 import { Requests } from "../src/Pages/Requests/Requests";
 function App() {
-  
-
   return (
     <>
       <BrowserRouter>
-      <Routes>
-        
-          <Route  index element={< Home/>}/>
+        <Routes>
+          <Route index element={<Home />} />
 
-          <Route path="products" element={<Products/>} />
+          <Route path="products" element={<Products />} />
 
-          <Route path="categories" element={<Categories/>}/>
+          <Route path="categories" element={<Categories />} />
 
-          <Route path="requests" element={<Requests />}/>
-      </Routes>
-    </BrowserRouter>
+          <Route path="requests" element={<Requests />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
