34bc466121e6bf92abedb9133f84610b33d7e91b
Carlosss <carlos2019cavalcante@gmail.com>
2024-07-22 19:00:10 -0300

    seção de item em destaque pronta

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import { Header } from "../../Components/Header/Header";
 import { Carousel } from "../../Components/Carousel/Carousel";
 import { Footer } from "../../Components/Footer/Footer";
+import { Featureds } from "../../Components/FeaturedSection/Featureds";
 export function Home() {
   
   return (
     <>
       <Header/>
       <Carousel/>
+      <Featureds/>
       <Footer/>
     </>
   )
