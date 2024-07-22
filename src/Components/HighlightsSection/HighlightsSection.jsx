22af668e88fbbee8e5c416b087b747be85496f7a
Carlosss <carlos2019cavalcante@gmail.com>
2024-07-21 22:54:07 -0300

    primeiros cards feitos, o pai trabalha pra caralho, queria um emprego

--- src/Components/HighlightsSection/HighlightsSection.jsx
+++ src/Components/HighlightsSection/HighlightsSection.jsx
@@ -9,11 +9,24 @@ export const HighlightsSection = () => {
         <div className="content-text">
           <span className="subtitle">Coleções em destaque</span>
         </div>
-        <div className="content-cards">
-          <PromoCard mods={"promoCard"}/>
-          <PromoCard mods={"promoCard"}/>
-          <PromoCard mods={"promoCard"}/>
+        <div className="position-cards">
+          <PromoCard mods={"promoCard"} 
+          promoValue={"30% OFF"} 
+          promoName={"Novo drop Supreme"}
+          imgSrc={"src/assets/collection-1.png"}
 
+          />
+          <PromoCard mods={"promoCard"}
+          promoValue={"30% OFF"} 
+          promoName={"Coleção Adidas"}
+          imgSrc={"src/assets/collection-2.png"}
+          />
+          <PromoCard mods={"promoCard"} 
+          promoValue={"30% OFF"} 
+          promoName={"Novo Beat Bass"}
+          imgSrc={"src/assets/collection-3.png"}
+          imgDetails={"headset-img"}
+          />
         </div>
       </div>
       <div className="highlights-icons">
